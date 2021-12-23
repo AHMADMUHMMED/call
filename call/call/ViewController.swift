cb4ffb9824c501e2c4ef925ac699eca3736ae1c2
AHMADMUHMMED <gtx-1990@hotmail.com>
2021-12-23 16:28:04 +0300

    ss

---
+++ call/call/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  call
+//
+//  Created by Ahmad Barqi on 19/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
